c23085caa2586ede1aee421cf8e9ea500f674cce
sigai <vipfts@qq.com>
2019-04-03 16:31:46 +0800

    :art:

--- utils/export_id.py
+++ utils/export_id.py
@@ -12,8 +12,23 @@ def export(collection):
             r.sadd("itslaw:id", jid)
             print(f"[+] {jid}")
 
+def dump(count):
+    r = redis.Redis()
+    ret = set()
+    for i in range(count):
+        item = r.spop("itslaw:id")
+        jid = str(item, encoding="utf-8")
+        ret.add(jid)
+    with open("ids.txt", mode="w", encoding="utf-8") as f:
+        f.write("\n".join(ret))
 
+def load():
+    r = redis.Redis()
+    with open("ids.txt", mode="r", encoding="utf-8") as f:
+        for line in f:
+            jid = line.strip()
+            r.sadd("itslaw:id", jid)
 
 
 if __name__ == "__main__":
-    export("law_anhui")
+    dump(100)
